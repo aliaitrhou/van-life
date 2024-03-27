9d676ff2d0146a0eddbd485db49ee34204b2dc85
aliaitrahou <145855773+aliaitrahou@users.noreply.github.com>
2024-03-27 14:52:16 +0000

    adding firebase authentication.

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,10 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration, replace it with your project keys
 const firebaseConfig = {
-  apiKey: "AIzaSyAr4miEyeFDCgWcpgLOKPIdRirU5j1XKKU",
-  authDomain: "vanl-f8b34.firebaseapp.com",
-  projectId: "vanl-f8b34",
-  storageBucket: "vanl-f8b34.appspot.com",
-  // messagingSenderId: "553108426000",
-  // appId: "1:553108426000:web:6c9b4403452de15f1a6995",
-  // measurementId: "G-GMLKK42TD4"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
 };
 
 // Initialize Firebase
